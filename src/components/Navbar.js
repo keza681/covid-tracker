dd5b8c8b31715957fe0d3de8e79b4fef1f4c9993
keza681 <lindakeza19@gmail.com>
2022-04-22 16:11:44 +0200

    Add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <nav>
+      <section className="navLinks">
+        <div>
+          <NavLink to="./">
+            <img src="https://img.icons8.com/ios-filled/24/ffffff/back.png" alt="back" />
+          </NavLink>
+        </div>
+        <div className="mainTitle">
+          <NavLink to="/">
+            Covid 19 total cases.
+          </NavLink>
+        </div>
+        <div className="navIcons">
+          <img className="navIcon" src="https://img.icons8.com/fluency-systems-filled/24/ffffff/microphone.png" alt="mic" />
+          <img className="navIcon" src="https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/24/ffffff/external-setting-essentials-pack-tanah-basah-glyph-tanah-basah.png" alt="settings" />
+        </div>
+      </section>
+    </nav>
+  );
+}
+
+export default NavBar;
